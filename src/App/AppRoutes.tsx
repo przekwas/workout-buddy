94afad20357c0a9e7f7a378b169d3c6aebeabffa
Luke Przkwas <luke@covalence.io>
2023-05-30 22:42:06 -0500

    adding new routes and more navbar code

--- src/App/AppRoutes.tsx
+++ src/App/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Home } from '@/views';
+import { Home, Login, Register } from '@/views';
 
 export default function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
 		</Routes>
 	);
 }
--- src/shared/components/Navbar/index.tsx
+++ src/shared/components/Navbar/index.tsx
@@ -1,10 +1,27 @@
+import { Link, NavLink } from 'react-router-dom';
+
 import Container from '../Container';
 
 export default function Navbar() {
 	return (
-		<div className="navbar bg-primary text-primary-content">
+		<div className="navbar">
 			<Container>
-				<a className="text-xl normal-case btn btn-ghost">daisyUI</a>
+				<div className="flex-1">
+					<Link to="/" className="text-xl normal-case btn btn-ghost">
+						WorkoutBuddy
+					</Link>
+				</div>
+				<div className="flex-none">
+					<ul className="px-1 menu menu-horizontal">
+						<li>
+							<NavLink
+								to="/login"
+								className={({ isActive }) => (isActive ? 'active' : '')}>
+								Login
+							</NavLink>
+						</li>
+					</ul>
+				</div>
 			</Container>
 		</div>
 	);
